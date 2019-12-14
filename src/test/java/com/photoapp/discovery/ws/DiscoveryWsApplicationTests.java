7ee7632651328cd869374d351640460eb1a100d8
Priyak Dey <priyak1991dey@gmail.com>
2019-12-15 02:23:24 +0530

    Initial Commit

---
+++ src/test/java/com/photoapp/discovery/ws/DiscoveryWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoapp.discovery.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
